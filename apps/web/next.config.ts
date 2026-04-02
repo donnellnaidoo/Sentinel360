888ca011f8190c524e90641a83a28434a4c281c0
Donnell Naidoo <donnellnaidoo@Donnells-MacBook-Air.local>
2026-04-02 08:01:22 +0200

    initial commit

---
+++ apps/web/next.config.ts
@@ -0,0 +1,9 @@
+import "@Sentinel360/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  reactCompiler: true,
+};
+
+export default nextConfig;
